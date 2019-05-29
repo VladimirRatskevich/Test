e0f7b1baa12e98d0cfedf3aafd0d1bfa0070ab97
VladimirRatskevich <51114769+VladimirRatskevich@users.noreply.github.com>
2019-05-29 12:30:09 +0300

    Создано несколько функций

--- Gradle training/src/main/java/by/RJcode/First.java
+++ Gradle training/src/main/java/by/RJcode/First.java
@@ -3,7 +3,26 @@ package by.RJcode;
 public class First {
 
   public static void main(String[] args) {
-    System.out.println("Olololo");
+    hello("valodya");
+
+    double len = 5;
+    System.out.println("Площадь kvadrata so storonoy " + len + " = " + area(len));
+
+    double a = 4;
+    double b = 5;
+    System.out.println("Ploshad' pramougolnika so storonami " + a + " and " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Olololo " + somebody + "!");
+  }
+
+  public static double area (double l) {
+    return  l * l;
+  }
+
+  public static double area (double a, double b) {
+    return a*b;
   }
 
 }
